40f5193daee8aace7e9e2010444c4f7d8f329026
John Ostap <crumatoc@gmail.com>
2016-06-08 18:35:10 -0400

    Added Stylus support

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 
 //Remember, environment variable. Not for production.
 app.use(session({
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Frag Hub', loggedIn: loggedIn(req) });
+  res.render('index', { loggedIn: loggedIn(req) });
 });
 
 const loggedIn = function(req, res) {
